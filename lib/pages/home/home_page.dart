c604141b3940c199d2897956eaf81ad5478e64d7
TungLam111 <38311934+TungLam111@users.noreply.github.com>
2021-02-08 14:18:09 +0700

    complete app architecture

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// 3 bottom bars
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) ;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
