44f811b5404e983b9ecf8d8c29e88f543fb015b1
TungLam111 <38311934+TungLam111@users.noreply.github.com>
2021-02-27 22:24:57 +0700

    Revert "description.dart"
    This reverts commit 6892d9a137cbeb5480fc3a032dbfbd335af1ab56.

--- lib/pages/desciption.dart
+++ lib/pages/desciption.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home:Description(),
+  ));
+}
 class Description extends StatelessWidget{
   @override
   Widget build(BuildContext context){
