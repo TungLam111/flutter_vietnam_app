98486adeb4fac967ac734fe629e5b5033b8f5df1
TungLam111 <38311934+TungLam111@users.noreply.github.com>
2021-06-16 19:39:57 +0700

    add youtube video view

--- lib/common/widgets/pages/page_product.dart
+++ lib/common/widgets/pages/page_product.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vietnam_app/common/widgets/pages/page_product_2.dart';
 import 'package:flutter_vietnam_app/models/location.dart';
+import 'package:flutter_vietnam_app/pages/home/home_screen/youtube.dart';
+
 class DescriptionProduct extends StatefulWidget {
   final Location location;
   const DescriptionProduct({this.location});
@@ -11,16 +13,17 @@ class DescriptionProduct extends StatefulWidget {
 
 class _DescriptionProductState extends State<DescriptionProduct> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         children: [
-          NewWidget(location: widget.location,),
+          NewWidget(
+            location: widget.location,
+          ),
           CloneBooking(location: widget.location)
         ],
         scrollDirection: Axis.vertical,
       ),
-
     );
   }
 }
@@ -28,162 +31,199 @@ class _DescriptionProductState extends State<DescriptionProduct> {
 class NewWidget extends StatefulWidget {
   final Location location;
   const NewWidget({this.location});
- @override
-_NewWidgetState createState() => _NewWidgetState();
+  @override
+  _NewWidgetState createState() => _NewWidgetState();
 }
 
 class _NewWidgetState extends State<NewWidget> {
-  final PageController _pageController = PageController(initialPage: 0, keepPage: true);
+  final PageController _pageController =
+      PageController(initialPage: 0, keepPage: true);
   int _currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<Object>(
-      stream: null,
-      builder: (context, snapshot) {
-        return Scaffold(
-                   resizeToAvoidBottomInset: true,
-                      resizeToAvoidBottomPadding: true,
-          body: Stack(
-            children:[
-              
-              PageView.builder(
-                 onPageChanged: (value){
-            setState(() {
-              _currentPage = value;  
-            });
-          },
-          controller: _pageController,
-          itemCount: widget.location.images.length,
-          itemBuilder :(context,index ){
-            return   Container(
-          decoration: BoxDecoration(
-          image:DecorationImage(
-            image: NetworkImage(widget.location.images[index]),
-            fit:BoxFit.cover,
-          )
-              ),
-              );
-          }
-          
-            ,),
-              
-              Container(
-          decoration: BoxDecoration(
-            boxShadow:[
-              BoxShadow(color:Colors.black12, spreadRadius:0.5),
-            ],
-          gradient: LinearGradient(
-            colors: [Colors.black12, Colors.black87],
-            begin: Alignment.center,
-            stops: [0.2, 1],
-            end: Alignment.bottomCenter,
-            ),
-          ),
-          child:Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children:[
-                    SizedBox(height:30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                      GestureDetector(
-                        child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          color: Colors.white),
-                        padding: EdgeInsets.all(12),
-                        child: Icon(Icons.arrow_back_ios_rounded, size: 15)
+        stream: null,
+        builder: (context, snapshot) {
+          return Scaffold(
+              resizeToAvoidBottomInset: true,
+              resizeToAvoidBottomPadding: true,
+              body: Stack(children: [
+                PageView.builder(
+                  onPageChanged: (value) {
+                    setState(() {
+                      _currentPage = value;
+                    });
+                  },
+                  controller: _pageController,
+                  itemCount: widget.location.images.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == widget.location.images.length)
+                      return Youtube();
+                    return Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: NetworkImage(widget.location.images[index]),
+                        fit: BoxFit.cover,
                       )),
-                       GestureDetector(
-                        child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          color: Colors.white),
-                        padding: EdgeInsets.all(12),
-                        child: Icon(Icons.favorite_outline_rounded, size: 15)
-                      ))
-                    ],),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                      GestureDetector(child: Icon(Icons.arrow_back_ios_rounded, color: Colors.white), onTap: (){
-                         setState(() {
-                            _currentPage =  (_currentPage > 0) ? _currentPage-1 : 0;  
-                            _pageController.animateToPage(
-                              _currentPage,
-                              duration: Duration(
-                                milliseconds: 200,
-                              ),
-                              curve: Curves.easeIn,
-                            );
-            });
-                      }),
-                      SizedBox(width: 20,),
-                       GestureDetector(child: Icon(Icons.arrow_forward_ios_rounded, color: Colors.white), onTap: (){
-                         setState(() {
-                            _currentPage =  (_currentPage < 3) ? _currentPage + 1 : 3;  
-                            _pageController.animateToPage(
-                              _currentPage,
-                              duration: Duration(
-                                milliseconds: 200,
-                              ),
-                              curve: Curves.easeIn,
-                            );
-                         });
-                       })
-                    ],),
-                    SizedBox(height: 10,),
-                    Text(widget.location.name,
-                    style:TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize:30,
-                    )),
-                    SizedBox(height: 10,),
-                    Text(widget.location.origin,
-                    style:TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize:15,
-                    )),
-                    SizedBox(height: 10,),
-                    Text(widget.location.description,
-                    maxLines: 3,
-                    style:TextStyle(
-                      color: Colors.white,
-                      fontSize:15,
-                    )),
-                    SizedBox(height: 10,),
-                    Row(children: [
-
-                    ],),
-                    InkWell(
-                      onTap: (){},
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Column(
-                          children: [
-                            Text('Xem thêm',style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize:15,
-                            ),),
-                            Icon(Icons.keyboard_arrow_up,color:Colors.white,size:30)
-                          ],
-                        ),
+                    );
+                  },
+                ),
+                Positioned(
+                 // alignment: Alignment.bottomCenter,
+                 bottom:0, right: 0, left: 0,
+                  child: Container(
+                //    height: 250,
+                      decoration: BoxDecoration(
+                        //  boxShadow:[
+                        //   BoxShadow(color:Colors.black12, spreadRadius:0.5),
+                        // ],
+                        gradient: LinearGradient(
+                          colors: [Colors.transparent, Colors.black87],
+                          begin: Alignment.center,
+                          stops: [0.2, 1],
+                          end: Alignment.bottomCenter,
+                          ),
+                       ),
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              //mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                // Spacer(),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    GestureDetector(
+                                        child: Icon(
+                                            Icons.arrow_back_ios_rounded,
+                                            color: Colors.white),
+                                        onTap: () {
+                                          setState(() {
+                                            _currentPage = (_currentPage > 0)
+                                                ? _currentPage - 1
+                                                : 0;
+                                            _pageController.animateToPage(
+                                              _currentPage,
+                                              duration: Duration(
+                                                milliseconds: 200,
+                                              ),
+                                              curve: Curves.easeIn,
+                                            );
+                                          });
+                                        }),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    GestureDetector(
+                                        child: Icon(
+                                            Icons.arrow_forward_ios_rounded,
+                                            color: Colors.white),
+                                        onTap: () {
+                                          setState(() {
+                                            _currentPage = (_currentPage <
+                                                    widget.location.images
+                                                            .length +
+                                                        1)
+                                                ? _currentPage + 1
+                                                : widget.location.images.length;
+                                            _pageController.animateToPage(
+                                              _currentPage,
+                                              duration: Duration(
+                                                milliseconds: 200,
+                                              ),
+                                              curve: Curves.easeIn,
+                                            );
+                                          });
+                                        })
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(widget.location.name,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30,
+                                    )),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(widget.location.origin,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    )),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(widget.location.description,
+                                    maxLines: 3,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    )),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                               
+                                InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Xem thêm',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                        Icon(Icons.keyboard_arrow_up,
+                                            color: Colors.white, size: 30)
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ]))),
+                ),
+                Align(
+                    child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10.0)),
+                                      color: Colors.white),
+                                  padding: EdgeInsets.all(12),
+                                  child: Icon(Icons.arrow_back_ios_rounded,
+                                      size: 15))),
+                          GestureDetector(
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10.0)),
+                                      color: Colors.white),
+                                  padding: EdgeInsets.all(12),
+                                  child: Icon(Icons.favorite_outline_rounded,
+                                      size: 15)))
+                        ],
                       ),
-                    )
-                    ]
-                )
-          )
-          )
-            ])
-        );
-      }
-    );
+                    ],
+                  ),
+                ))
+              ]));
+        });
   }
 }
